e255bbba4fe24d7df495e424b417690ff94b250b
ricardodutra <ric.dalvarez@hotmail.com>
2020-08-11 11:55:51 +0200

    fix bugs with push

--- funfood/Server/controllers/controllers.js
+++ funfood/Server/controllers/controllers.js
@@ -3,10 +3,7 @@ const restaurantModel = require('../models/RestaurantModel');
 async function getAll(_, res) {
     try {
         const restaurants = await restaurantModel.find();
-<<<<<<< HEAD
-=======
 
->>>>>>> 5782893df9001e05c47c15fc971f833347d2f34d
         res.status(200);
         res.json(restaurants);
     } catch (error) {
@@ -16,11 +13,7 @@ async function getAll(_, res) {
 }
 async function createOne(req, res) {
     try {
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 5782893df9001e05c47c15fc971f833347d2f34d
         const restaurant = await restaurantModel.create(
             {
                 name: req.body.name,
@@ -38,10 +31,7 @@ async function createOne(req, res) {
     }
 }
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 5782893df9001e05c47c15fc971f833347d2f34d
 async function updateOne(req, res) {
     try {
         const updateRestaurant = await restaurantModel.findOneAndUpdate(
@@ -63,11 +53,5 @@ async function updateOne(req, res) {
 }
 
 
-
-<<<<<<< HEAD
-module.exports = { getAll, createOne, updateOne }
-=======
 module.exports = { getAll, createOne, updateOne }
 
-
->>>>>>> 5782893df9001e05c47c15fc971f833347d2f34d
